9bcd2ae237e5d8385382e0156aa097f2128eeb2b
Jordon Replogle <jordonreplogle@gmail.com>
2016-12-11 00:34:35 -0800

    Day 7, part 1

--- DaySeven/src/DaySeven.hx
+++ DaySeven/src/DaySeven.hx
@@ -4,18 +4,16 @@ import parsers.*;
 
 using StringTools;
 
-class DayThree {
+class DaySeven {
   private static inline var inputPath:String = "input.txt";
   //private static inline var inputPath:String = "inputTest.txt";
   private static var input:String = "";
 
   static public function main() {
     input = sys.io.File.getContent(inputPath).trim();
-
-    var row:Rows = new Rows(input);
-    var column:Columns = new Columns(input);
-    trace(row.parse());
-    trace(column.parse());
-
+    
+    var ips:IP7 = new IP7(input);
+    trace(ips.valid());
+    
   }
 }
