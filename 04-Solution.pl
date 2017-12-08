9b369516260a98aecce799d9395356af65bff664
Jordon Replogle <jordon.replogle@blueletterbible.org>
2017-12-08 15:05:44 -0800

    Day 4 Part 2

--- 04-Solution.pl
+++ 04-Solution.pl
@@ -30,10 +30,56 @@ for my $row (@lines) {
     if($count == 0) {
         $valid++;
     }
-
-
 }
 
 print "="x20 . "\n";
 print "Part 1: $valid\n";
+print "="x20 . "\n";
+
+# ***
+# Part 2 -
+# For example:
+# abcde fghij is a valid passphrase.
+# abcde xyz ecdab is not valid - the letters from the third word can be rearranged to form the first word.
+# a ab abc abd abf abj is a valid passphrase, because all letters need to be used when forming another word.
+# iiii oiii ooii oooi oooo is valid.
+# oiii ioii iioi iiio is not valid - any of these words can be rearranged to form any other word.
+# ***
+
+my $valid = 0;
+
+for my $row (@lines) {
+    my @colVals = split(/\s/, $row);
+    my $count = 0;
+    my $numWords = @colVals;
+    my @sortedVals = &sortWords(@colVals);
+
+    for($i=0; $i<$numWords; $i++) {
+        for($j=$i+1; $j<$numWords; $j++) {
+            if(@sortedVals[$i] eq @sortedVals[$j]) {
+                $count++;
+            }
+        }
+    }
+
+    if($count == 0) {
+        $valid++;
+    }
+}
+
+sub sortWords {
+    my @words = @_;
+    my @sortedWords = ();
+
+    my $c = 0;
+    foreach my $w (@words) {
+        push(@sortedWords, join('', sort(split(//, $w))));
+        $c++;
+    }
+
+    return @sortedWords;
+}
+
+print "="x20 . "\n";
+print "Part 2: $valid\n";
 print "="x20 . "\n";
