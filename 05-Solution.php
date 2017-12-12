cf2fd26427cbd8ce1a93bae6570ccb6976a2d92b
Jordon Replogle <jordonreplogle@gmail.com>
2017-12-11 20:42:15 -0800

    Day 5 Part 2

--- 05-Solution.php
+++ 05-Solution.php
@@ -2,13 +2,14 @@
 <?php
 
 $inputPath = "./inputs/day5.txt";
-//$inputPath = "./inputs/temp.txt";
 
 $lines = file($inputPath);
 
 $inputs = array_map(function($v) {
     return (int) $v;
 }, $lines);
+
+$input2 = $inputs;
 
 $index = 0;
 $c = 0;
@@ -17,10 +18,26 @@ $instrLen = count($inputs);
 
 while($index >= 0 && $index < $instrLen) {
     $temp = $inputs[$index];
-    echo "$c: $index => $temp\n";
     $inputs[$index]++;
     $index += $temp;
     $c++;
 }
 
+print "====================\n";
 print $c;
+print "\n====================\n";
+
+$index = 0;
+$c = 0;
+$temp = 0;
+
+while($index >= 0 && $index < $instrLen) {
+    $temp = $input2[$index];
+    $input2[$index] += ($temp >= 3 ? -1 : 1);
+    $index += $temp;
+    $c++;
+}
+
+print "====================\n";
+print $c;
+print "\n====================\n";
