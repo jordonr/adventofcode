9f42b3ec1f690f9328d7bb7b80664653c3ecac7d
Jordon Replogle <jordon.replogle@blueletterbible.org>
2017-12-12 14:58:38 -0800

    Day 6 Part 1

---
+++ 06-Solution.php
@@ -0,0 +1,48 @@
+#!/usr/bin/php
+<?php
+//This proves I'm bad at math and should increase my knowledge of the subject
+
+$input = "0	5	10	0	11	14	13	4	11	8	8	7	1	4	12	11";
+//$input = "0 2   7   0"; //test data
+
+$prevStates = [];
+$banks = preg_split("/\s+/", $input);
+$dupState = FALSE;
+$count = 0;
+
+while(!$dupState) {
+    $key = array_search(max($banks), $banks);
+
+    $banks = spread($key, $banks);
+    $tempState = join("", $banks);
+
+    if(in_array($tempState, $prevStates)) {
+        $dupState = TRUE;
+    }
+
+    $prevStates[] = $tempState;
+    $count++;
+}
+
+print $count;
+
+function spread($startKey, $memBanks) {
+    $loops = $memBanks[$startKey];
+    $memBanks[$startKey] = 0;
+
+    $c = 1;
+    while($loops > 0) {
+        if(isset($memBanks[$startKey+$c])) {
+            $memBanks[$startKey+$c]++;
+        } else {
+            $c = 0; 
+            $startKey = 0;
+            $memBanks[0]++;
+        }
+
+        $c++;
+        $loops--;
+    }
+
+    return $memBanks;
+}
