9a49218aabe8c1c296d90546110c1604c5e44559
Jordon Replogle <jordonreplogle@gmail.com>
2018-12-09 23:11:14 -0800

    Day 05, Part 2

--- Day-05/Day05.hx
+++ Day-05/Day05.hx
@@ -1,43 +1,78 @@
-package ;
+package;
 
 using StringTools;
 
 class Day05 {
-  private static inline var inputPath:String = "../inputs/Day5.txt";
-//   private static inline var inputPath:String = "../inputs/Day5-Test.txt";
-  private static var input:String = "";
+    private static inline var inputPath:String = "../inputs/Day5.txt";
+    // private static inline var inputPath: String = "../inputs/Day5-Test.txt";
+    private static var input: String = "";
 
-  static public function main() {
-    input = sys.io.File.getContent(inputPath).trim();
+    static public function main() {
+        input = sys.io.File.getContent(inputPath).trim();
 
-    var remainder:String = polyermize(input);
+        // Part 1
+        var remainder: String = polyermize(input);
+        Sys.println("Part 1: " + remainder.length);
 
-    trace(remainder.length);
-  }
+        // Part 2
+        Sys.println("Part 2:");
+        var lowLen: Int = input.length;
+        var output: String = "";
+        var reducedPoly = unique(input);
+        for (char in reducedPoly) {
+            var rahc: String = flipCase(char);
+            var newChain: String = StringTools.replace(input, char, "");
+            newChain = StringTools.replace(newChain, rahc, "");
 
-  private static function polyermize(polyString:String) : String {
-      var retPoly:String = polyString;
-      var beenMatched:Bool = false;
-      for(c in 0...polyString.length-1) {
-        if(polyString.charAt(c) == flipCase(polyString.charAt(c+1))) {
-            var temp:String = polyString.charAt(c) + polyString.charAt(c+1);
-            retPoly = StringTools.replace(retPoly, temp, "");
-            beenMatched = true;
+            var mod: String = polyermize(newChain);
+
+            Sys.println("  " + char + ": " + mod.length);
+
+            if(mod.length < lowLen) {
+                output = "Winner - " + char + ": " + mod.length;
+                lowLen = mod.length;
+            }
         }
-      }
 
-      if(beenMatched) {
-          retPoly = polyermize(retPoly);
-      }
+        Sys.println(output);
+    }
 
-      return retPoly;
-  }
+    private static function polyermize(polyString: String): String {
+        var retPoly: String = polyString;
+        var beenMatched: Bool = false;
+        for (c in 0...polyString.length - 1) {
+            if (polyString.charAt(c) == flipCase(polyString.charAt(c + 1))) {
+                var temp: String = polyString.charAt(c) + polyString.charAt(c + 1);
+                retPoly = StringTools.replace(retPoly, temp, "");
+                beenMatched = true;
+            }
+        }
 
-  private static function flipCase(char:String) : String {
-      if(char.toUpperCase() == char) {
-          return char.toLowerCase();
-      } else {
-          return char.toUpperCase();
-      }
-  }
+        if (beenMatched) {
+            retPoly = polyermize(retPoly);
+        }
+
+        return retPoly;
+    }
+
+    private static function flipCase(char: String): String {
+        if (char.toUpperCase() == char) {
+            return char.toLowerCase();
+        } else {
+            return char.toUpperCase();
+        }
+    }
+
+    private static function unique(myStr: String): Array<String> {
+        var retArray: Array<String> = new Array<String>();
+
+        for (c in 0...myStr.length) {
+            var upperChar: String = myStr.charAt(c).toUpperCase();
+            if(retArray.indexOf(upperChar) == -1) {
+                retArray.push(upperChar);
+            }
+        }
+
+        return retArray;
+    }
 }
--- README.md
+++ README.md
@@ -36,8 +36,10 @@ This is my repo for going through the 2018 Advent of Code.
 
 | Day           | Notes   |
 |:------------- |:------- |
-| Running Haxe  | * Interpeter: `haxe -main Day-05 --interpet`
-                  * Neko: `haxe -main Day-05 -neko day05.n`
-                  * CPP Linux: `haxe -main Day-05 -cpp bin`
-                  * CPP Windows: 1) `haxelib install minimingw` 2)`haxe -main Day-05 -cpp bin -lib minimingw`|
 | Day 01        | Took 140 loops through the given input to find the answer.  Python took 2m13.898s, so while that was going, I thought I'd try Haxe using the same logic.  Haxe complied to cpp target took 0m8.356s to execute. |
+
+### Running Haxe  
+* Interpeter: `haxe -main Day-05 --interpet`
+* Neko: `haxe -main Day-05 -neko day05.n`
+* CPP Linux: `haxe -main Day-05 -cpp bin`
+* CPP Windows: 1) `haxelib install minimingw` 2)`haxe -main Day-05 -cpp bin -lib minimingw`
