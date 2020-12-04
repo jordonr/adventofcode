1cff830cfd5f37567eb34b50137bbb35cdb5a790
Jordon Replogle <jordon.replogle@blueletterbible.org>
2020-12-04 12:44:27 -0800

    Finished Day 04

--- 02-Solution/Day02.hx
+++ 02-Solution/Day02.hx
@@ -4,73 +4,72 @@ using Math;
 using StringTools;
 
 /****
-Part 1 -
-Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a 
-given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
+	Part 1 -
+	Each line gives the password policy and then the password. The password policy indicates the lowest and highest number of times a
+	given letter must appear for the password to be valid. For example, 1-3 a means that the password must contain a at least 1 time and at most 3 times.
 
-Part 2 - 
-Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; 
-Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter.
+	Part 2 -
+	Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful;
+	Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter.
 ***/
-
 class Day02 {
-    private static inline var inputPath:String = "../inputs/Day02.txt";
-    // private static inline var inputPath: String = "../inputs/Day02-Test.txt";
-    private static var input: String = "";
+	private static inline var inputPath:String = "../inputs/Day02.txt";
+	// private static inline var inputPath: String = "../inputs/Day02-Test.txt";
+	private static var input:String = "";
 
-    static public function main() {
-        input = sys.io.File.getContent(inputPath).trim();
-        var lineRegex = ~/\n/g;
-        var lines: Array < String > = lineRegex.split(input);
+	static public function main() {
+		input = sys.io.File.getContent(inputPath).trim();
+		var lineRegex = ~/\n/g;
+		var lines:Array<String> = lineRegex.split(input);
 
-        Sys.println('Part 1: ' + partOne(lines));
-        Sys.println('Part 2: ' + partTwo(lines));
-    }
+		Sys.println('Part 1: ' + partOne(lines));
+		Sys.println('Part 2: ' + partTwo(lines));
+	}
 
-    private static function partOne(lines:Array<String>): Int {
-        var pwRegex = ~/(\d+)-(\d+)\s(\w):\s(\w+)/g;
-        var valid = 0;
+	private static function partOne(lines:Array<String>):Int {
+		var pwRegex = ~/(\d+)-(\d+)\s(\w):\s(\w+)/g;
+		var valid = 0;
 
-        for (i in 0...lines.length) {
-            pwRegex.match(lines[i]);
-            var charCount = 0;
-            var password = pwRegex.matched(4);
-            var testChar = pwRegex.matched(3);
-            var max = Std.parseInt(pwRegex.matched(2));
-            var min = Std.parseInt(pwRegex.matched(1));
+		for (i in 0...lines.length) {
+			pwRegex.match(lines[i]);
+			var charCount = 0;
+			var password = pwRegex.matched(4);
+			var testChar = pwRegex.matched(3);
+			var max = Std.parseInt(pwRegex.matched(2));
+			var min = Std.parseInt(pwRegex.matched(1));
 
-            for(c in 0...password.length) {
-                if(password.charAt(c) == testChar) {
-                    charCount++;
-                }
-            }
+			for (c in 0...password.length) {
+				if (password.charAt(c) == testChar) {
+					charCount++;
+				}
+			}
 
-            if(charCount >= min && charCount <= max) {
-                valid++;
-            }
-        }
+			if (charCount >= min && charCount <= max) {
+				valid++;
+			}
+		}
 
-        return valid;
-    }
+		return valid;
+	}
 
-    private static function partTwo(lines:Array<String>): Int {
-        var pwRegex = ~/(\d+)-(\d+)\s(\w):\s(\w+)/g;
-        var valid = 0;
+	private static function partTwo(lines:Array<String>):Int {
+		var pwRegex = ~/(\d+)-(\d+)\s(\w):\s(\w+)/g;
+		var valid = 0;
 
-        for (i in 0...lines.length) {
-            pwRegex.match(lines[i]);
-            var password = pwRegex.matched(4);
-            var testChar = pwRegex.matched(3);
-            var matchOne = Std.parseInt(pwRegex.matched(2)) - 1;
-            var matchTwo = Std.parseInt(pwRegex.matched(1)) - 1;
+		for (i in 0...lines.length) {
+			pwRegex.match(lines[i]);
+			var password = pwRegex.matched(4);
+			var testChar = pwRegex.matched(3);
+			var matchOne = Std.parseInt(pwRegex.matched(2)) - 1;
+			var matchTwo = Std.parseInt(pwRegex.matched(1)) - 1;
 
-            if((password.charAt(matchOne) == testChar && password.charAt(matchTwo) != testChar) || 
-                password.charAt(matchTwo) == testChar && password.charAt(matchOne) != testChar) {
-                valid++;
-            }
+			if ((password.charAt(matchOne) == testChar && password.charAt(matchTwo) != testChar)
+				|| password.charAt(matchTwo) == testChar
+				&& password.charAt(matchOne) != testChar) {
+				valid++;
+			}
+		}
 
-        }
-
-        return valid;
-    }
+		return valid;
+	}
 }
--- 04-Solution/Day04.hx
+++ 04-Solution/Day04.hx
@@ -16,7 +16,17 @@ using haxe.Int64;
 	cid (Country ID) (Optional)
 
 	Part 2 -
-	These slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+	byr (Birth Year) - four digits; at least 1920 and at most 2002.
+	iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+	eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+	hgt (Height) - a number followed by either cm or in:
+		If cm, the number must be at least 150 and at most 193.
+		If in, the number must be at least 59 and at most 76.
+	hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+	ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+	pid (Passport ID) - a nine-digit number, including leading zeroes.
+	cid (Country ID) - ignored, missing or not.
+
 ***/
 class Day04 {
 	private static inline var inputPath:String = "../inputs/Day04.txt";
@@ -25,18 +35,18 @@ class Day04 {
 
 	static public function main() {
 		input = sys.io.File.getContent(inputPath).trim();
-        // var lineRegex = ~/\n\n/g;
-        var lineRegex = ~/\r\n\r\n/g; //Windows
+		var lineRegex = ~/\n\n/g;
+		// var lineRegex = ~/\r\n\r\n/g; // Windows
 		var passports:Array<String> = lineRegex.split(input);
 
 		Sys.println('Part 1: ' + partOne(passports));
-		// Sys.println('Part 2: ' + product);
+		Sys.println('Part 2: ' + partTwo(passports));
 	}
 
 	private static function partOne(passports:Array<String>):Int {
 		var valid = 0;
-        var requiredFields = 7;
-        var fieldRegex = ~/(\w{3}):/g;
+		var requiredFields = 7;
+		var fieldRegex = ~/(\w{3}):/g;
 
 		for (p in 0...passports.length) {
 			var validFields = [
@@ -49,25 +59,103 @@ class Day04 {
 				"pid" => 0
 			];
 
-            var pp = passports[p];
+			var pp = passports[p];
 			while (fieldRegex.match(pp)) {
-                var field = fieldRegex.matched(1);
-                if(validFields.exists(field)) {
-                    validFields[field] = 1;
-                }
+				var field = fieldRegex.matched(1);
+				if (validFields.exists(field)) {
+					validFields[field] = 1;
+				}
 				pp = fieldRegex.matchedRight();
-            }
+			}
 
-            var fieldTest = 0;
-            for(v in validFields) {
-                fieldTest += v;
-            }
+			var fieldTest = 0;
+			for (v in validFields) {
+				fieldTest += v;
+			}
 
-            if(fieldTest == requiredFields) {
-                valid++;
-            }
+			if (fieldTest == requiredFields) {
+				valid++;
+			}
 		}
 
+		return valid;
+	}
+
+	private static function partTwo(passports:Array<String>):Int {
+		var valid = 0;
+		var requiredFields = 7;
+		var fieldRegex = ~/(\w{3}):([\w\d#]+)/g;
+
+		for (p in 0...passports.length) {
+			var validFields = [
+				"byr" => 0,
+				"iyr" => 0,
+				"eyr" => 0,
+				"hgt" => 0,
+				"hcl" => 0,
+				"ecl" => 0,
+				"pid" => 0
+			];
+			var validEyeColor = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+			var pp = passports[p];
+			while (fieldRegex.match(pp)) {
+				var field = fieldRegex.matched(1);
+				var value = fieldRegex.matched(2);
+
+				switch field {
+					case "byr": // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+						var year:Int = Std.parseInt(value);
+						if (year >= 1920 && year <= 2002) {
+							validFields[field] = 1;
+						}
+					case "iyr": // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+						var year:Int = Std.parseInt(value);
+						if (year >= 2010 && year <= 2020) {
+							validFields[field] = 1;
+						}
+					case "eyr": // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+						var year:Int = Std.parseInt(value);
+						if (year >= 2020 && year <= 2030) {
+							validFields[field] = 1;
+						}
+					case "hgt": // hgt (Height) - a number followed by either cm or in:
+						var height:Int = Std.parseInt(value);
+						// If cm, the number must be at least 150 and at most 193.
+						// If in, the number must be at least 59 and at most 76.
+						if ((value.endsWith("cm") && height >= 150 && height <= 193)
+							|| (value.endsWith("in") && height >= 59 && height <= 76)) {
+							validFields[field] = 1;
+						}
+					case "hcl": // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+						var hexColorRegex = ~/^#[a-f0-9]{6}/g;
+						if (hexColorRegex.match(value) && value.length == 7) {
+							validFields[field] = 1;
+						}
+					case "ecl": // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+						if (validEyeColor.contains(value)) {
+							validFields[field] = 1;
+						}
+					case "pid": // pid (Passport ID) - a nine-digit number, including leading zeroes.
+						var pidRegex = ~/^\d{9}/g;
+						if (pidRegex.match(value) && value.length == 9) {
+							validFields[field] = 1;
+						}
+					default:
+						// trace("Ignored Field: " + field + ": " + value);
+				}
+				pp = fieldRegex.matchedRight();
+			}
+
+			var fieldTest = 0;
+			for (v in validFields) {
+				fieldTest += v;
+			}
+
+			if (fieldTest == requiredFields) {
+				valid++;
+			}
+		}
 		return valid;
 	}
 }
