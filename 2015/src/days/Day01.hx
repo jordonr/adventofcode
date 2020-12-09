72d6857375ee91a556c0d33577705e34de2b66ad
Jordon Replogle <jordonreplogle@gmail.com>
2020-12-08 22:06:58 -0800

    2015 - Day 01

--- 2015/src/days/Day01.hx
+++ 2015/src/days/Day01.hx
@@ -6,7 +6,7 @@ class Day01 {
 	public function new(path:String) {
 		var data:String = ReadData.getFile(path);
 		Sys.println('Part 1: ' + partOne(data));
-		// Sys.println('Part 2: ' + partTwo(instructions));
+		Sys.println('Part 2: ' + partTwo(data));
 	}
 
 	private function partOne(data:String):Int {
@@ -21,5 +21,23 @@ class Day01 {
 		}
 
 		return floor;
+	}
+
+	private function partTwo(data:String):Int {
+		var floor:Int = 0;
+
+		for (c in 0...data.length) {
+			if (data.charAt(c) == "(") {
+				floor++;
+			} else if (data.charAt(c) == ")") {
+				floor--;
+			}
+
+			if (floor == -1) {
+				return c + 1;
+			}
+		}
+
+		return 0;
 	}
 }
