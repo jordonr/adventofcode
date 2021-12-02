542cf9d2062c0a20e0a3b3085eccbf44788e3d01
Jordon Replogle <jordon.replogle@blueletterbible.org>
2021-12-02 09:55:54 -0800

    Day 2, Part 2

--- 2021/src/days/Day02.hx
+++ 2021/src/days/Day02.hx
@@ -7,17 +7,23 @@ import utils.ReadData;
 
 /****
 	Part 1 -
-	Count the number of times a depth measurement increases from the previous measurement.
+	forward X increases the horizontal position by X units.
+	down X increases the depth by X units.
+	up X decreases the depth by X units.
+
 	Part 2 -
-	Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. 
-	So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+	down X increases your aim by X units.
+	up X decreases your aim by X units.
+	forward X does two things:
+		It increases your horizontal position by X units.
+		It increases your depth by your aim multiplied by X.
 ***/
 class Day02 {
 	public function new(path:String) {
 		var lines:Array<String> = ReadData.getLines(path);
 
 		Sys.println('Part 1: ' + partOne(lines));
-		// Sys.println('Part 2: ' + partTwo(lines));
+		Sys.println('Part 2: ' + partTwo(lines));
 	}
 
 	private function partOne(lines:Array<String>):Int {
@@ -35,10 +41,21 @@ class Day02 {
 		return x * y;
 	}
 
-	private function partTwo(numbers:Array<Int>):Int {
-		var deeper:Int = 0;
+	private function partTwo(lines:Array<String>):Int {
+		var x:Int = 0;
+		var y:Int = 0;
+		var aim:Int = 0;
 
-		return deeper;
+		for (line in lines) {
+			var lineParts:Array<String> = line.split(" ");
+			var coords:Array<Int> = getAdvCoords(lineParts, aim);
+
+			x = x + coords[0];
+			y = y + coords[1];
+			aim = coords[2];
+		}
+
+		return x * y;
 	}
 
 	private function getCoords(direction:Array<String>):Array<Int> {
@@ -59,5 +76,27 @@ class Day02 {
 		}
 
 		return [x, y];
+	}
+
+	private function getAdvCoords(direction:Array<String>, currentAim:Int):Array<Int> {
+		var x:Int = 0;
+		var y:Int = 0;
+		var aim:Int = currentAim;
+		var move:Int = Std.parseInt(direction[1]);
+
+		// up, down, forward
+		switch (direction[0]) {
+			case "down":
+				aim = aim + move;
+			case "up":
+				aim = aim - move;
+			case "forward":
+				x = x + move;
+				y += aim * move;
+			case "backward":
+				x = x - move;
+		}
+
+		return [x, y, aim];
 	}
 }
