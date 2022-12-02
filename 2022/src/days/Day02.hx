e3535ac5d084ed5e4adc23ffb93057d755ab472f
Jordon Replogle <jordon.replogle@blueletterbible.org>
2022-12-02 12:38:02 -0800

    2022: Day 2, Part 2

--- 2022/src/days/Day02.hx
+++ 2022/src/days/Day02.hx
@@ -8,7 +8,9 @@ import utils.ReadData;
 class Day02 {
 
 	// Rock, Paper, Scissors
-	private var rpsValues:Map<String, Int> = [ "A" => 1, "B" => 2, "C" => 3, "X" => 1, "Y" => 2, "Z" => 3];
+	private var rpsValues:Map<String, Int> = ["A" => 1, "B" => 2, "C" => 3, "X" => 1, "Y" => 2, "Z" => 3];
+	private var winMoves:Map<String, String> = ["A" => "B", "B" => "C", "C" => "A"];
+	private var failMoves:Map<String, String> = ["A" => "C", "B" => "A", "C" => "B"];
 	
 	public function new(path:String) {
 		var lines:Array<String> = ReadData.getLines(path);
@@ -26,7 +28,6 @@ class Day02 {
 			
 			total += rpsValues[me];
 			
-			// Tie
 			if(rpsValues[me] == rpsValues[them]) {
 				total += 3;	
 			} else if((rpsValues[me] == 1 && rpsValues[them] == 3) ||
@@ -41,10 +42,29 @@ class Day02 {
 	}
 
 	private function partTwo(lines:Array<String>):Int {
-		var x:Int = 0;
-		var y:Int = 0;
+		var total:Int = 0;
 
+		for(game in lines) {
+			var them = game.charAt(0);
+			var winLooseDraw = game.charAt(2);
+			var me = "";
+			
+			if(rpsValues[winLooseDraw] == 2) {
+				// Tie
+				total += 3;	
+				me = them;
+			} else if(rpsValues[winLooseDraw] == 1) { 
+				// Loose
+				me = failMoves[them];
+			} else if(rpsValues[winLooseDraw] == 3) { 
+				// Win
+				total += 6;
+				me = winMoves[them];
+			} 
 
-		return x * y;
+			total += rpsValues[me];
+		}
+
+		return total;
 	}
 }
