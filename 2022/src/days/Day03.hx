37568a60e2ab2af066f63a0ff2a42577db48ca1d
Jordon <jordonr@pop-os.localdomain>
2022-12-03 15:13:55 -0800

    2022, Day 3, Part 1

--- 2022/src/days/Day03.hx
+++ 2022/src/days/Day03.hx
@@ -6,21 +6,64 @@ using StringTools;
 import utils.ReadData;
 
 /****
-	Part 1 -
-	Part 2 -
+ * The list of items for each rucksack is given as characters all on a single line. 
+ * A given rucksack always has the same number of items in each of its two compartments, 
+ * so the first half of the characters represent items in the first compartment, 
+ * while the second half of the characters represent items in the second compartment.
+ * 
+ * To help prioritize item rearrangement, every item type can be converted to a priority:
+ * Lowercase item types a through z have priorities 1 through 26.
+ * Uppercase item types A through Z have priorities 27 through 52.
+ * 7333 too low
 ***/
+
 class Day03 {
+	var alphas:String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	var alphaValues:Map<String, Int> = [];
+	
 	public function new(path:String) {
 		var lines:Array<String> = ReadData.getLines(path);
+		//var lines:Array<String> = [
+		//"vJrwpWtwJgWrhcsFMMfFFhFp",
+		//"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+		//"PmmdzqPrVvPwwTWBwg",
+		//"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+		//"ttgJtRGJQctTZtZT",
+		//"CrZsJsPPZsGzwwsLwLmpwMDw"
+		//];
+		
+		for(c in 0...alphas.length) {
+			alphaValues.set(alphas.charAt(c), c+1);
+		}
 
-		Sys.print('Part 1: ');
-		partOne(lines, true);
+		Sys.println('Part 1: ' + partOne(lines));
 		Sys.println('Part 2: ' + partTwo(lines));
 	}
 
-	private function partOne(lines:Array<String>, printOutput:Bool = false):Array<Array<Int>> {
+	private function partOne(lines:Array<String>):Int {
+		var total:Int = 0;
+		
+		for(l in lines) {
+			var halfway:Int = Std.int(l.length / 2);
+			var pocket1 = l.substring(0, halfway);
+			var pocket2 = l.substring(halfway, l.length);
+			var uniquePocket:Array<String> = [];
+			
+			for(p in 0...halfway) {
+				var item = pocket1.charAt(p);
+				if(uniquePocket.indexOf(item) == -1) {
+					uniquePocket.push(item);
+				}
+			}
+			
+			for(c in 0...uniquePocket.length) {
+				if(pocket2.indexOf(uniquePocket[c]) > -1) {
+					total += alphaValues[uniquePocket[c]];
+				}
+			}
+		}
 
-		return [0, 0];
+		return total;
 	}
 
 	private function partTwo(lines:Array<String>):Int {
