1b9220780109558f8141e0469bd66e99406e7cef
Jordon Replogle <jordonreplogle@gmail.com>
2024-12-09 14:48:01 -0500

    Day 02, Part 2

--- 2024/Package.swift
+++ 2024/Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "2023",
+    name: "2024",
     dependencies: [
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "2023"),
+            name: "2024"),
     ]
 )
 
--- 2024/Sources/main.swift
+++ 2024/Sources/main.swift
@@ -4,4 +4,4 @@
 let day = Day02()
 
 day.Part1()
-// day.Part2()
+day.Part2()
