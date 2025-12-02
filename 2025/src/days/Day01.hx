f5c4d3bc6bbc325e42bcbb56a1cba6123ad4a566
jordonr <jordonreplogle@gmail.com>
2025-12-01 23:13:39 -0500

    2025: Day 01, Part 2

--- 2025/src/days/Day01.hx
+++ 2025/src/days/Day01.hx
@@ -10,7 +10,7 @@ class Day01 {
 		var lines:Array<String> = ReadData.getLinesTrimmed(path);
 
 		Sys.println('Part 1: ' + partOne(lines));
-		// Sys.println('Part 2: ' + partTwo(numbers));
+		Sys.println('Part 2: ' + partTwo(lines));
 	}
 
 	private function partOne(lines:Array<String>):Int {
@@ -27,7 +27,7 @@ class Day01 {
 				current -= clicks;	
 			}
 
-			if(current%100 == 0) {
+			if(current%max == 0) {
 				count++;
 			}
 		}
@@ -35,9 +35,32 @@ class Day01 {
 		return count;
 	}
 
-	private function partTwo(numbers:Array<Int>):Int {
-		var countCals:Int = 0;
+	private function partTwo(lines:Array<String>):Int {
+		var current:Float = start;
+		var count:Int = 0;
 		
-		return countCals;
+		for(l in lines) {
+			var dir = l.substring(0, 1);
+			var value = Std.parseInt(l.substring(1, l.length));
+			var clicks = value%100;
+			var check = current;
+
+			count += Std.int(value/max);
+			
+			if(dir == "R") {
+				check = max - current;
+				current += clicks;
+			} else if(dir == "L") {
+				current -= clicks;
+			}
+
+			current = (max + current%max)%max;
+
+			if(clicks >= check && check != 0) {
+				count++;
+			}
+		}
+
+		return count;
 	}
 }
