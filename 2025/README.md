5cda1bf44e9b1485e5a5d702c2b4823c2c605140
jordonr <jordonreplogle@gmail.com>
2025-12-05 22:18:39 -0500

    2025: Day 04, Part 1

--- 2025/README.md
+++ 2025/README.md
@@ -9,7 +9,7 @@ This is my repo for going through the 2025 Advent of Code.
 | Day 01 |  **   |  Haxe    | Day01.hx |
 | Day 02 |  **   |  Haxe    | Day02.hx |
 | Day 03 |  **   |  Haxe    | Day03.hx |
-| Day 04 |  |   |  |
+| Day 04 |  *   |  Haxe    | Day04.hx |
 | Day 05 |  |   |  |
 | Day 06 |  |   |  |
 | Day 07 |  |   |  |
@@ -24,6 +24,7 @@ This is my repo for going through the 2025 Advent of Code.
 | Day    | Notes  |
 | :----- | :----- |
 | Day 02 | I had to use Int64, simple brute force solution, but slow |
+| Day 03 | More Int64 |
 
 ### Running Haxe
 
--- 2025/src/days/Day04.hx
+++ 2025/src/days/Day04.hx
@@ -1,11 +1,27 @@
 package days;
 
+import haxe.exceptions.ArgumentException;
 import utils.ReadData;
 
 class Day04 {
 
+	private var _grid:Array<Array<String>>;
+	private var _height:Int;
+	private var _width:Int;
+	private var _search_coords:Array<Array<Int>>;
+
 	public function new(path:String) {
 		var lines:Array<String> = ReadData.getLinesTrimmed(path);
+		_search_coords = [
+			[-1, -1],
+			[0, -1],
+			[1, -1],
+			[-1, 0],
+			[1, 0],
+			[-1, 1],
+			[0, 1],
+			[1, 1]
+		];
 
 		Sys.println('Part 1: ' + partOne(lines));
 		// Sys.println('Part 2: ' + partTwo(lines));
@@ -13,9 +29,20 @@ class Day04 {
 
 	private function partOne(lines:Array<String>):Int {
 		var count:Int = 0;
+		_grid = [];
 		
 		for(l in lines) {
-            trace(l);
+            _grid.push(l.split(""));
+		}
+
+		_height = _grid.length;
+		_width = _grid[0].length;
+		for(y in 0..._height) {
+			for(x in 0..._width) {
+				if(_grid[y][x] == "@" && fourTest(x, y)) {
+					count++;
+				}
+			}
 		}
 
 		return count;
@@ -25,5 +52,28 @@ class Day04 {
 		var count:Int = 0;
 		
 		return count;
+	}
+
+	private function fourTest(x:Int, y:Int):Bool {
+		var tp:Array<String> = [];
+
+		for(i in 0...8) {
+			tp.push(safeGetValue(x + _search_coords[i][0], y + _search_coords[i][1]));
+		}
+
+		if(tp.filter(n -> n == "@").length < 4) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private function safeGetValue(x:Int, y:Int):String {
+
+		if(x < 0 || y < 0 || x > _width - 1 || y > _height - 1) {
+			return ".";
+		}
+
+		return _grid[y][x];
 	}
 }
